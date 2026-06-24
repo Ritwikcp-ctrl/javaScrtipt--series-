ee704e38e045d716b49daa1ae42371d6e3ea5767
Sunny  Rai <su30rai213@gmail.com>
2026-06-24 12:39:52 +0000

    operaions performed

---
+++ .github/01_basics/test3.js
@@ -0,0 +1,28 @@
+//Here we will stydy:- opertaions
+
+let value = 4
+let negValue = -value;
+
+console.log("1" + 2); // 12
+console.log(1 + "2");//12
+console.log(1+2+"2");//32
+console.log("1" +2 +2);//122
+ 
+
+// Prefix and Postfix concept is very simple. 
+
+/////////////now about the conditional statements /////////
+
+
+ console.log(null>0) // false
+ console.log(null == 0)// false
+console.log(null >= 0)// true
+ // The reason is that an equality check== and comparison < > >= <= 
+ //work differently.console.
+ //Comparisons convert null to number . treating it as 0 . 
+ //thats why (3) null >=0  is true and (1) null >0 is false .
+ // Now check with undefined  in all the above three cases it will always return false.
+ 
+
+ // === (This is called as Strict check , means it check the data type .)
+ 
