04262417727b470df7e2607bf5e9ce8bc46677c7
Sunny  Rai <su30rai213@gmail.com>
2026-06-25 11:30:39 +0000

    Date class and etc.

---
+++ .github/01_basics/test6.js
@@ -0,0 +1,24 @@
+// in this we worked in consoel of  the browser.
+
+const name = "sunny"
+const repoCount = 94
+
+console.log(`My name is ${name} and  repocount is ${repoCount}`)
+
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+
+const otherNumber = 21.8977
+console.log(otherNumber.toPrecision(2))
+
+//let talk about the maths :-
+// There is various mathods in the maths library that we use in language 
+//just call  the Math.___();
+
+const myDa = new Date();
+// Date is a class and myDa is the instance object of the Date class.
