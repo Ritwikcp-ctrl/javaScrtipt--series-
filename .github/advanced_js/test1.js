b0c5a1463ca5c572010ed302552b9d905acd46bd
Sunny  Rai <su30rai213@gmail.com>
2026-07-01 03:29:53 +0000

    few consist of js code and oher with html DOM.

---
+++ .github/advanced_js/test1.js
@@ -0,0 +1,12 @@
+//Learning about the DOM :=
+// document.getElementById("firstHeading")
+// <h1 id=​"firstHeading" class=​"firstHeading mw-first-heading">​…​</h1>​
+// document.getElementById("firstHeading").innerHTML = "<h1> Sunny Rai  </h1>"
+// '<h1> Sunny Rai  </h1>'
+// document.getElementById("mw-content-text").innerHTML = " <p> This is called DOM manipulation ,can only be done if you have deep understanding of Programming Language </p>"
+// ' <p> This is called DOM manipulation ,can only be done if you have deep understanding of Programming Language </p>'
+ 
+
+// Lets check the difference between the innerText , textContent and innerHTML
+document.querySelector('input[type = "password"]')
+//it return the complete input field .
