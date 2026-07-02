ddaf6fbb9c1d7723a11709183f777ede8361d36a
Sunny  Rai <su30rai213@gmail.com>
2026-07-02 05:41:01 +0000

    bmi calculator

---
+++ .github/project01/bmi.js
@@ -0,0 +1,26 @@
+// we are going to build a dom manipulated project called body mass calculator :
+
+const form =   document.querySelector('form')
+form.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+    const height = parseInt(document.querySelector('height').value)
+    const weight = parseInt(document.querySelector('weight').value)
+    const result = document.querySelector('result')
+
+    if(height ==='' || height<0 || isNaN(height)){
+        result.innerHTML = `plese give a valid height ${height}`
+    }
+    else if(weight === '' ||weight <0 || isNaN(weight)){
+        result.innerHTML = `Pls give a valid weight ${weight}`
+    } else {
+        const bmi = (weight/((height*height)/1000)).toFixed(2)
+
+        result.innerHTML = `<span> ${bmi} </span>`
+    }
+
+    
+})
+
+
+
